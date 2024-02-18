acb9ca6d8c1a8b257d9ca1995be3f3143e38c255
Max-im <160244375+MaxLutsenko205@users.noreply.github.com>
2024-02-19 00:06:12 +0600

    Learning day 1

---
+++ src/src/code_examples.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Code_examples {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+/*
+        Ternary operator:
+
+        variable = (condition)?true result:false result;
+        int a = 4;
+        String output = (a % 2 == 0) ? "even" : "odd";
+        System.out.println(output);
+*/
+
+/*
+        Switch cases
+
+        switch (variable) {
+          case condition -> result;
+          case condition -> result;
+          default -> result;
+        }
+        int x = s.nextInt();
+        int y = s.nextInt();
+        String text = s.next();
+        switch (text) {
+            case "+" -> System.out.println(x+y);
+            case "-" -> System.out.println(x-y);
+            case "*" -> System.out.println(x*y);
+            case "/" -> System.out.println(x/y);
+            default -> System.out.println("Invalid operator");
+        }
+*/
+
+
+
+    }
+}
